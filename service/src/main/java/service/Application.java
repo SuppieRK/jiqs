1f37ab7dd14c0d7c8f07d7447b35b0ce56630cc4
Roman Khlebnov <suppie.rk@gmail.com>
2025-02-15 09:24:35 +0100

    Documentation update

--- service/src/main/java/service/Application.java
+++ service/src/main/java/service/Application.java
@@ -4,9 +4,22 @@ import platform.ApplicationWrapper;
 import service.endpoints.UsersEndpointGroup;
 import service.services.UsersService;
 
+/** Starting point. */
 public class Application {
+  /**
+   * In this simple example we leave this field visibility at package-default level for easier
+   * access in tests.
+   *
+   * <p>It could be a good idea to expose this field via public getter - just make sure that your
+   * implementation of {@link ApplicationWrapper} is immutable.
+   */
   final ApplicationWrapper wrapper;
 
+  /**
+   * Default constructor.
+   *
+   * <p>Also registers all necessary dependencies.
+   */
   public Application() {
     this.wrapper =
         ApplicationWrapper.createApplication()
@@ -14,6 +27,11 @@ public class Application {
             .build();
   }
 
+  /**
+   * Default main method.
+   *
+   * @param args to pass to the app
+   */
   public static void main(String[] args) {
     final var application = new Application();
 
